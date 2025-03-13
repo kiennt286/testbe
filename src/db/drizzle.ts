07c73865846cefc8eb674631f449b94798143f0d
kien78 <tkienn286@gmail.com>
2025-03-13 07:42:00 +0700

    Fix database connection

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -12,6 +12,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 3306,
 })
-
-
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
 export const db = drizzle(pool, { schema, mode: "default" });
