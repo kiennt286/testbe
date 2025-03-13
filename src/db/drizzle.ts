dbee272f9eeca8eac0fab50689816113b7fbc4d5
kien78 <tkienn286@gmail.com>
2025-03-13 07:46:42 +0700

    Fix DB connection issue on Railway

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,8 +1,6 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
-import * as dotenv from "dotenv";
-dotenv.config(); 
 
 console.log("🛠️ Loaded ENV:", process.env.DB_HOST)
 const pool = mysql.createPool({
